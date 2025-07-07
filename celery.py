5f45ce1b5376d8d423f6b248f5e85e6a86a327c0
eddiegulay <edgargulay@outlook.com>
2025-07-07 18:41:45 +0300

    Add Dockerfile and requirements for Celery and Redis setup

--- celery.py
+++ celery.py
@@ -1,16 +1,26 @@
 from celery import Celery
+from celery.schedules import crontab
 
-app = Celery(
-    'briqBackgroundTasks',
+celery_app = Celery(
+    'Briq Celery',
     broker='redis://:${REDIS_PASSWORD}@redis:6379/0',
     backend='redis://:${REDIS_PASSWORD}@redis:6379/0',
-    include=[]
+    broker_connection_retry_on_startup=True,
+    include=[]  # Empty if tasks are defined externally
 )
 
-app.conf.update(
+celery_app.conf.update(
     task_serializer='json',
     accept_content=['json'],
     result_serializer='json',
-    timezone='UTC',
     enable_utc=True,
+    result_expires=300,
 )
+
+# Celery Beat schedule for cron jobs
+celery_app.conf.beat_schedule = {
+    'process-scheduled-messages': {
+        'task': 'app.workers.message.process_scheduled_messages_task',
+        'schedule': crontab(minute='*/5'),  # Refresh every 5 minutes
+    },
+}
